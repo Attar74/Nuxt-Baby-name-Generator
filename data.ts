3a7584d212b7fe6665e09934c2a0118511647999
Attar74 <m.elattar.dev@gmail.com>
2023-11-30 03:04:18 +0200

    first commit

---
+++ data.ts
@@ -0,0 +1,267 @@
+interface Name {
+    id: number;
+    name: string;
+    gender: Gender;
+    popularity: Popularity;
+    length: Length;
+  }
+  
+  export enum Gender {
+    GIRL = "Girl",
+    BOY = "Boy"
+  }
+  
+  export enum Popularity {
+    TRENDY = "Trendy",
+    UNIQUE = "Unique",
+  }
+  
+  export enum Length {
+    SHORT = "Short",
+    LONG = "Long",
+    ALL = "All",
+  }
+  export const names: Name[] = [
+    {
+      id: 1,
+      name: "Moaz",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 2,
+      name: "Youssef",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 3,
+      name: "Noah",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 4,
+      name: "Ebrahim",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 5,
+      name: "mo`men",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 6,
+      name: "Houd",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 7,
+      name: "Mohammed",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 8,
+      name: "Youssef",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 9,
+      name: "Salem",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 10,
+      name: "Abdelmo`men",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 11,
+      name: "Salma",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 12,
+      name: "Ashery",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 13,
+      name: "Eyad",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+  
+    {
+      id: 14,
+      name: "Menna",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 15,
+      name: "Yara",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 16,
+      name: "Mara",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 17,
+      name: "Yasmina",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 18,
+      name: "Genevieve",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 19,
+      name: "Zoe",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 20,
+      name: "Valentina",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 21,
+      name: "Josephine",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 22,
+      name: "Maram",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 23,
+      name: "Everleigh",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 24,
+      name: "Poppy",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 25,
+      name: "Mai",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 26,
+      name: "Amina",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+  
+    {
+      id: 27,
+      name: "Jude",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 28,
+      name: "Adrian",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 29,
+      name: "Sunny",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 30,
+      name: "Channing",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 31,
+      name: "Tennessee",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 32,
+      name: "Dallas",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+  
+    {
+      id: 33,
+      name: "Zephyr",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+  
+    {
+      id: 34,
+      name: "Teri",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+  ];
